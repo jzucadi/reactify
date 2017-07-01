bcb051dc55b76d503203bbf2573b78a3ef9e534a
James Zaccardo <jameszaccardo@gmail.com>
2017-07-01 03:08:44 -0700

    Setting up levels

---
+++ src/Components/Projects.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Projects extends Component {
+    render() {
+        return ( <
+            div className = "Projects" >
+            <
+            p > My Projects < /p> <
+            /div>
+        );
+    }
+}
+
+export default Projects;
