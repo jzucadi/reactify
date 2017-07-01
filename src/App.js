82c589e164d286aaa5b87ab5c942d0c6aa847f8e
James Zaccardo <jameszaccardo@gmail.com>
2017-07-01 03:26:58 -0700

    constructor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
-import Projects from "./Component/Projects";
+import Projects from "./Components/Projects";
 import "./App.css";
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            projects: [{
+                title: 'Business Website',
+                category: 'Web Something'
+            }]
+        }
+    }
     render() {
         return ( <
             div className = "App" >
             <
-            p > My App < /p> <
-            Projects / >
+            p > My App < /p> <Projects / >
             <
             /div>
         );
