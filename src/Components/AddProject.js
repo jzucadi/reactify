d0eca60dcf0ff770e80f0f6bbc4c1eb3cd6debb5
James Zaccardo <jameszaccardo@gmail.com>
2017-07-02 21:27:34 -0700

    removed styles

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
 
 class AddProject extends Component {
+  static defaultProps = {
+      categories: ['Web Design', 'Web Development', 'Mobile Development']
+  }
+  
   render() {
+    let categoryOptions = this.props.categories.map(category => {
+        return <option key={category} value="category">{category}</option>
+    });
     return (
       <div>
         <h3>Add Project</h3>
         <form>
           <div>
-            <label>Title</label>
+            <label>Title</label><br />
             <input type="text" ref="" />
           </div>
+          <div>
+            <label>Category</label><br />
+            <select ref="category">
+                {categoryOptions}
+                </select>
+         </div>
         </form>
       </div>
     );
