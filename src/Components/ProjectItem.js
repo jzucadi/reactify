67ed47e6bea702b757867b96a9dadd9664656099
James Zaccardo <jameszaccardo@gmail.com>
2017-07-03 21:27:56 -0700

    main props

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 
 class ProjectItems extends Component {
+  deleteProject(){
+    console.log('test');
+  }
+  
   render() {
     return (
       <li className="Project">
-        <strong>{this.props.project.title}</strong> : {this.props.project.category}
+        <strong>{this.props.project.title}</strong> : {this.props.project.category} <a href="#" onClick={this.deleteProject.bind(this)}>X</a>  
       </li>
     );
   }
