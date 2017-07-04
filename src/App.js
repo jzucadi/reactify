9a77eefab2b79c264626f475659ccb0a66d6d7b0
James Zaccardo <jameszaccardo@gmail.com>
2017-07-03 22:11:17 -0700

    added XHR

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import uuid from 'uuid';
+import $ from 'jquery';
 import Projects from "./Components/Projects";
 import AddProject from "./Components/AddProject";
 
@@ -9,12 +10,25 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      projects: []
+      projects: [],
+      todos:[]
     }
   }
 
   getTodos(){
-
+    $.ajax({
+      url: 'https://jsonplaceholder.typicode.com/todos',
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({todos: data}, function(){
+          console.log(this.state);
+        })
+      }.bind(this),
+      error:function(xhr, status, err){
+        console.log(err);
+      }
+    })
   }
 
   getProjects(){
