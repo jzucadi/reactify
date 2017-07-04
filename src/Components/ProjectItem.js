795fdeeebccaf8df067c34f616bb236e0de9aa4c
James Zaccardo <jameszaccardo@gmail.com>
2017-07-03 21:31:51 -0700

    delete option

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 
 class ProjectItems extends Component {
-  deleteProject(){
-    console.log('test');
+  deleteProject(id){
+    this.props.onDelete(id);
   }
   
   render() {
     return (
       <li className="Project">
-        <strong>{this.props.project.title}</strong> : {this.props.project.category} <a href="#" onClick={this.deleteProject.bind(this)}>X</a>  
+        <strong>{this.props.project.title}</strong> : {this.props.project.category} <a href="#" onClick={this.deleteProject.bind(this, this.props.project.id)}>X</a>  
       </li>
     );
   }
